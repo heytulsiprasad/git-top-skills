8e6e68c787c8fa917540cdba8926b50fa2a6f0ba
Tulsi Prasad <tulsi.prasad50@gmail.com>
2023-10-10 19:20:40 +0530

    feat: add more blogs

--- main.js
+++ main.js
@@ -1,13 +1,49 @@
 console.log("Hey")
 
-const values = [
-    { heading: "TITLE HEADING", description: "Title description", date: "April 7, 2014", body: "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sedultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.", comments: "12", image: "/images/woods.jpg" },
-    { heading: "TITLE HEADING", description: "Title description", date: "April 7, 2014", body: "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sedultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.", comments: "12", image: "/images/woods.jpg" },
-    { heading: "TITLE HEADING", description: "Title description", date: "April 7, 2014", body: "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sedultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.", comments: "12", image: "/images/woods.jpg" },
-    { heading: "TITLE HEADING", description: "Title description", date: "April 7, 2014", body: "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sedultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.", comments: "12", image: "/images/woods.jpg" },
-]
+const data = [
+  {
+    heading: "Exploring Nature's Beauty",
+    description: "A journey into the wilderness",
+    date: "July 15, 2022",
+    body: "Embarking on a journey into the heart of nature is a breathtaking experience. From lush forests to serene lakes, there's a world of wonder waiting to be discovered. Get ready to immerse yourself in the beauty of the great outdoors.",
+    comments: "23",
+    image: "/images/woods.jpg"
+  },
+  {
+    heading: "The Art of Cooking",
+    description: "A culinary adventure",
+    date: "March 9, 2022",
+    body: "Cooking is not just a necessity; it's an art form. Whether you're a seasoned chef or a novice in the kitchen, there's always something new to learn. Join us on a culinary adventure as we explore flavors, techniques, and the joy of creating delicious meals.",
+    comments: "37",
+    image: "/images/woods.jpg"
+  },
+  {
+    heading: "The Road Less Traveled",
+    description: "Discovering hidden gems",
+    date: "November 20, 2021",
+    body: "Sometimes, the most memorable journeys are the ones that take you off the beaten path. Join us as we venture into lesser-known destinations, uncovering hidden gems, and embracing the beauty of serendipity.",
+    comments: "15",
+    image: "/images/woods.jpg"
+  },
+  {
+    heading: "Capturing Moments in Time",
+    description: "The art of photography",
+    date: "May 5, 2021",
+    body: "Photography is more than just snapping pictures; it's a way to freeze moments in time. Whether you're a professional photographer or a smartphone enthusiast, there's a story behind every click of the shutter.",
+    comments: "42",
+    image: "/images/woods.jpg"
+  },
+  {
+    heading: "A Taste of History",
+    description: "Exploring ancient cuisines",
+    date: "February 14, 2021",
+    body: "Delve into the past as we explore the culinary traditions of ancient civilizations. From Egyptian feasts to Roman banquets, we'll take you on a journey through time, one dish at a time.",
+    comments: "28",
+    image: "/images/woods.jpg"
+  }
+];
 
-console.log(values);
+// console.log(values);
 
 const createBlog = (heading, description, date, body, comments, image) => {
     return (
@@ -39,7 +75,7 @@ const createBlog = (heading, description, date, body, comments, image) => {
 
 const blogs = document.querySelector("#blogs")
 
-values.forEach((item) => {
+data.forEach((item) => {
     const blog = createBlog(item.heading, item.description, item.date, item.body, item.comments, item.image);
     const blogParent = document.createElement("div");
 
